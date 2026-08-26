bccc1b450bf97aaa46f3ad8317aef94018a0b602
SyntaxError <294848543+ValueMovedHere@users.noreply.github.com>
2026-08-26 13:20:40 +0800

    Use bevy_fps_controller

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,33 @@
+use std::f32::consts::TAU;
+
+use avian3d::prelude::*;
 use bevy::{
+    camera::Exposure,
     light::{CascadeShadowConfigBuilder, DirectionalLightShadowMap},
     prelude::*,
 };
+use bevy_fps_controller::controller::*;
+
+const SPAWN_POINT: Vec3 = Vec3::new(0.0, 1.625, 0.0);
 
 fn main() {
     App::new()
         .insert_resource(DirectionalLightShadowMap { size: 4096 })
         .add_plugins(DefaultPlugins)
+        .add_plugins(PhysicsPlugins::default())
+        .add_plugins(FpsControllerPlugin)
         .add_systems(Startup, load_scene)
+        .add_systems(Startup, setup_player)
+        .add_systems(Update, respawn)
         .run();
 }
 
 fn load_scene(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
-        Camera3d::default(),
-        Transform::from_xyz(10.7, 10.7, 11.0).looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::Y),
-    ));
-
-    commands.spawn((
         DirectionalLight {
             shadow_maps_enabled: true,
             ..default()
         },
-        // This is a relatively small scene, so use tighter shadow
-        // cascade bounds than the default for better quality.
-        // We also adjusted the shadow map to be larger since we're
-        // only using a single cascade.
         CascadeShadowConfigBuilder {
             num_cascades: 1,
             maximum_distance: 1.6,
@@ -36,4 +38,71 @@ fn load_scene(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(WorldAssetRoot(
         asset_server.load(GltfAssetLabel::Scene(0).from_asset("models/school_bp.glb")),
     ));
+}
+
+fn setup_player(mut commands: Commands) {
+    // Note that we have two entities for the player
+    // One is a "logical" player that handles the physics computation and collision
+    // The other is a "render" player that is what is displayed to the user
+    // This distinction is useful for later on if you want to add multiplayer,
+    // where often time these two ideas are not exactly synced up
+    let height = 3.0;
+    let logical_entity = commands
+        .spawn((
+            Collider::cylinder(0.5, height),
+            // A capsule can be used but is NOT recommended
+            // If you use it, you have to make sure each segment point is
+            // equidistant from the translation of the player transform
+            // Collider::capsule(0.5, height),
+            Friction {
+                dynamic_coefficient: 0.0,
+                static_coefficient: 0.0,
+                combine_rule: CoefficientCombine::Min,
+            },
+            Restitution {
+                coefficient: 0.0,
+                combine_rule: CoefficientCombine::Min,
+            },
+            LinearVelocity::ZERO,
+            RigidBody::Dynamic,
+            LockedAxes::ROTATION_LOCKED,
+            Mass(1.0),
+            GravityScale(0.0),
+            Transform::from_translation(SPAWN_POINT),
+            LogicalPlayer,
+            FpsControllerInput {
+                pitch: -TAU / 12.0,
+                yaw: TAU * 5.0 / 8.0,
+                ..default()
+            },
+            FpsController {
+                air_acceleration: 80.0,
+                ..default()
+            },
+        ))
+        .insert(CameraConfig {
+            height_offset: -0.5,
+        })
+        .id();
+
+    commands.spawn((
+        Camera3d::default(),
+        Projection::Perspective(PerspectiveProjection {
+            fov: TAU / 5.0,
+            ..default()
+        }),
+        Exposure::SUNLIGHT,
+        RenderPlayer { logical_entity },
+    ));
+}
+
+fn respawn(mut query: Query<(&mut Transform, &mut LinearVelocity)>) {
+    for (mut transform, mut velocity) in &mut query {
+        if transform.translation.y > -50.0 {
+            continue;
+        }
+
+        velocity.0 = Vec3::ZERO;
+        transform.translation = SPAWN_POINT;
+    }
 }
