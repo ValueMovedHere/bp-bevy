5accb2a899b7ef2143c6680fb796c44ee3353a19
SyntaxError <294848543+ValueMovedHere@users.noreply.github.com>
2026-08-27 15:45:59 +0800

    Add colliders to scene

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ fn main() {
         .add_plugins(PhysicsPlugins::default())
         .add_plugins(FpsControllerPlugin)
         .add_systems(Startup, load_scene)
+        .add_systems(Startup, set_scene_colliders)
         .add_systems(Startup, setup_player)
         .add_systems(Update, respawn)
         .run();
@@ -105,4 +106,95 @@ fn respawn(mut query: Query<(&mut Transform, &mut LinearVelocity)>) {
         velocity.0 = Vec3::ZERO;
         transform.translation = SPAWN_POINT;
     }
+}
+
+fn set_scene_colliders(mut commands: Commands) {
+    let colliders_cuboid = Collider::compound(vec![
+        (
+            Vec3::new(0.0, 0.0, 0.0), // floor
+            Quat::IDENTITY,
+            Collider::cuboid(4.290, 0.001, 27.320),
+        ),
+        (
+            Vec3::new(-1.148, 0.853, 14.883), // steps
+            Quat::from_rotation_x(-26.89f32.to_radians()),
+            Collider::cuboid(2.286, 0.001, 3.950),
+        ),
+        (
+            Vec3::new(1.194, 2.550, 15.301), // steps
+            Quat::from_rotation_x(25.91f32.to_radians()),
+            Collider::cuboid(2.282, 0.001, 3.692),
+        ),
+        (
+            // steps1
+            Vec3::new(1.148, 0.853, -14.883),
+            Quat::from_rotation_x(26.89f32.to_radians()),
+            Collider::cuboid(2.286, 0.001, 3.950),
+        ),
+        (
+            // steps1
+            Vec3::new(-1.194, 2.550, -15.301),
+            Quat::from_rotation_x(-25.91f32.to_radians()),
+            Collider::cuboid(2.282, 0.001, 3.692),
+        ),
+        (
+            // wall between steps
+            Vec3::new(0.000, 1.664, 15.187),
+            Quat::IDENTITY,
+            Collider::cuboid(0.317, 3.283, 3.113),
+        ),
+        (
+            // floor between steps
+            Vec3::new(0.000, 1.569, 17.602),
+            Quat::IDENTITY,
+            Collider::cuboid(4.662, 0.284, 1.949),
+        ),
+        (
+            // wall between steps
+            Vec3::new(0.000, 1.659, -15.185),
+            Quat::IDENTITY,
+            Collider::cuboid(0.286, 3.320, 3.097),
+        ),
+        (
+            // floor between steps
+            Vec3::new(0.000, 1.569, -17.602),
+            Quat::IDENTITY,
+            Collider::cuboid(4.662, 0.284, 1.949),
+        ),
+        (
+            // side wall
+            Vec3::new(2.224, 1.704, 0.000),
+            Quat::IDENTITY,
+            Collider::cuboid(0.171, 3.314, 36.825),
+        ),
+        (
+            // side wall
+            Vec3::new(-2.224, 1.704, 0.000),
+            Quat::IDENTITY,
+            Collider::cuboid(0.171, 3.314, 36.825),
+        ),
+    ]);
+    // let collider_side_wall = InfinitePlane3d::new(Vec3::new(1.000, 0.000, 0.000));
+    let collider_end_wall = InfinitePlane3d::new(Vec3::new(0.000, 0.000, 1.000));
+    commands.spawn((RigidBody::Static, colliders_cuboid));
+    // commands.spawn((
+    //     Transform::from_xyz(2.096, 0.000, 0.000),
+    //     RigidBody::Static,
+    //     Collider::from(collider_side_wall),
+    // ));
+    // commands.spawn((
+    //     Transform::from_xyz(-2.204, 0.000, 0.000),
+    //     RigidBody::Static,
+    //     Collider::from(collider_side_wall),
+    // ));
+    commands.spawn((
+        Transform::from_xyz(0.000, 0.000, 18.508),
+        RigidBody::Static,
+        Collider::from(collider_end_wall),
+    ));
+    commands.spawn((
+        Transform::from_xyz(0.000, 0.000, -18.508),
+        RigidBody::Static,
+        Collider::from(collider_end_wall),
+    ));
 }
