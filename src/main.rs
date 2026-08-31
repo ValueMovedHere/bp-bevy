d4515f2e034dc9a8c8164b2bd11c3592ca6b9c20
SyntaxError <294848543+ValueMovedHere@users.noreply.github.com>
2026-08-31 20:52:13 +0800

    State transition

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,31 @@ mod state;
 
 fn main() {
     App::new()
-        .init_state::<state::Level>()
         .insert_resource(DirectionalLightShadowMap { size: 4096 })
         .add_plugins(DefaultPlugins)
+        .init_state::<state::Level>()
         .add_plugins(PhysicsPlugins::default())
         .add_plugins(controller::FpsControllerPlugin)
-        .add_systems(Startup, levels::level_school::load_scene)
-        .add_systems(Startup, levels::level_school::set_scene_colliders)
-        .add_systems(Startup, levels::level_school::set_cursor)
+        .add_systems(
+            OnEnter(state::Level::LevelSchool),
+            levels::level_school::load_scene,
+        )
+        .add_systems(
+            OnEnter(state::Level::LevelSchool),
+            levels::level_school::set_scene_colliders,
+        )
+        .add_systems(
+            OnEnter(state::Level::LevelSchool),
+            levels::level_school::set_cursor,
+        )
         .add_systems(Startup, controller::setup_controller)
-        .add_systems(Update, (controller::respawn, state::manage_cursor))
+        .add_systems(
+            OnExit(state::Level::LevelSchool),
+            levels::level_school::cleanup,
+        )
+        .add_systems(
+            Update,
+            (controller::respawn, state::manage_cursor, state::next_state),
+        )
         .run();
 }
