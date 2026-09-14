735058bc01e06e37fe17baa9a1de4b88b5c849e1
SyntaxError <294848543+ValueMovedHere@users.noreply.github.com>
2026-09-14 20:49:36 +0800

    Add system to switch to next level on pressing key N

--- src/levels/level_backrooms.rs
+++ src/levels/level_backrooms.rs
@@ -2,6 +2,7 @@ use avian3d::prelude::*;
 use bevy::prelude::*;
 use serde_scene::from_json;
 
+use crate::Level;
 use crate::controller::INITIAL_VELOCITY;
 
 pub const SPAWN_POINT: Vec3 = Vec3::new(0f32, 0f32, 0f32);
@@ -36,3 +37,10 @@ pub fn respawn(query: Query<(&mut Transform, &mut LinearVelocity)>) {
 }
 
 pub fn cleanup() {}
+
+// 让我们假设在游戏中触发了切到下一级的逻辑
+pub fn next_scene(mut next_state: ResMut<NextState<Level>>, key: Res<ButtonInput<KeyCode>>) {
+    if key.just_pressed(KeyCode::KeyN) {
+        next_state.set(Level::LevelAbandonedvrgallery);
+    }
+}
