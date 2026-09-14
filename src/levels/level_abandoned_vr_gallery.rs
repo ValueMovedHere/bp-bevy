8dda8576708550e98f38201132f5ff6b41cdc4c3
SyntaxError <294848543+ValueMovedHere@users.noreply.github.com>
2026-09-14 21:48:33 +0800

    modified:   src/levels/level_abandoned_vr_gallery.rs

--- src/levels/level_abandoned_vr_gallery.rs
+++ src/levels/level_abandoned_vr_gallery.rs
@@ -12,14 +12,19 @@ pub struct LevelAbandonedVrGalleryRes;
 pub fn load_scene(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
         LevelAbandonedVrGalleryRes,
-        WorldAssetRoot(asset_server.load(GltfAssetLabel::Scene(0).from_asset(
-            "models/levels/level-abandoned-vr-gallery/level-abandoned-vr-gallery.glb",
-        ))),
+        WorldAssetRoot(
+            asset_server.load(
+                GltfAssetLabel::Scene(0).from_asset(
+                    "models/levels/level-abandoned-vr-gallery/abandoned-vr-gallery.glb",
+                ),
+            ),
+        ),
     ));
 }
 
 pub fn setup_colliders(mut commands: Commands) {
-    let colliders_vec = from_json("./data/levels/level-abandoned-vr-gallery/colliders.json");
+    let colliders_vec =
+        from_json("./data/levels/level-abandoned-vr-gallery/colliders/colliders.json");
     let scene_collider = Collider::compound(colliders_vec);
     commands.spawn((
         LevelAbandonedVrGalleryRes,
